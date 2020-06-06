216f75e2db637d096b0210c0aa7b7839abff9488
Diego Cordoba <diegocordoba87@hotmail.com>
2020-06-06 11:22:46 -0400

    updated readme with screenshots.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # password-generator
+This password generator has been created to help users create passwords of differet lengths between 8 and 128 characters.  Customers can choose from a different set of characters such as numbers, upper and lower case letter, and special characters.
 
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [MIT License](#MIT-License)
+* [Contributing](#Contributing)
+
+## Installation
+
+Clone repository on local machine and run HTML file in browser. After opening the app in the browser, click on generate password and follow the prompts at the top of the page.
+
+## Usage 
+The present application was made to help users create password of different lengths and characteristics without having to thin too much about how to get said random characters.
+
+[Deployed Portafolio Site](https://diegocordoba87.github.io/password-generator/)
+
+![Web App Upper ](assets/images/password_generator_main.png)
+![Web App Middle](assets/images/password_generator_prompt.png)
+![Web App Lower ](assets/images/password_generated.png)
+
+## Credits
+
+This was a single project completed as homework for Georgia Tech Coding Boot Camp /FullStack Flex Program. With the help of instructors and classmates, I was able to create this password generator.
+
+## MIT License
+
+Copyright (c) [2020] [Diego Cordoba]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
+
+## Contributing
+
+No contributions at this time. 
--- assets/script/script.js
+++ assets/script/script.js
@@ -7,59 +7,119 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  alert("We are about to create a password for you");
 
-function generatePassword(){
+  var specialArray = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+  var alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var password = "";
 
+  var passLength = prompt(
+    "Please enter a number between 8 and 128 to indicate how many characters you wish your password to have"
+  );
 
-alert('We are about to create a password for you');
+  while (passLength < 8 || passLength > 128) {
+    passLength = prompt("Invalid password length. Please try again");
+  }
 
-var specialArray = ["!", "\"", "#", "$", "%", "&", "\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","\`","{","|","}","~"];
-var alphabet = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var password = "";
+  var specialChars = confirm(
+    'Click "OK" if you wish to include SPECIAL characters'
+  );
+  var lowerChars = confirm(
+    'Click "OK" if you wish to include LOWERCASE characters'
+  );
+  var upperChars = confirm(
+    'Click "OK" if you wish to include UPPERCASE characters'
+  );
+  var numbers = confirm('Click "OK" if you wish to include NUMBERS characters');
 
-var passLength= prompt('Please enter a number between 8 and 128 to indicate how many characters you wish your password to have');
+  console.log(passLength);
 
-while(passLength< 8 || passLength >128){
-    passLength= prompt('Invalid password length. Please try again');
+  for (password.length = 0; password.length < passLength; password.length++) {
+    if (specialChars && password.length < passLength) {
+      var indexSpecial = Math.floor(Math.random() * specialArray.length);
+      password = password + specialArray[indexSpecial];
+    }
+    if (lowerChars && password.length < passLength) {
+      var indexLower = Math.floor(Math.random() * alphabet.length);
+      password = password + alphabet[indexLower];
+    }
+
+    if (upperChars && password.length < passLength) {
+      var indexUpper = Math.floor(Math.random() * alphabet.length);
+      password = password + alphabet[indexUpper].toUpperCase();
+    }
+
+    if (numbers && password.length < passLength) {
+      var indexNumber = Math.floor(Math.random() * numlist.length);
+      password = password + numlist[indexNumber];
+    }
+  }
+  return password;
 }
-
-var specialChars = confirm('Click "OK" if you wish to include SPECIAL characters');
-var lowerChars = confirm('Click "OK" if you wish to include LOWERCASE characters');
-var upperChars = confirm('Click "OK" if you wish to include UPPERCASE characters');
-var numbers = confirm('Click "OK" if you wish to include NUMBERS characters');
-
-console.log(passLength);
-
-
-for(password.length = 0; password.length < passLength; password.length ++){
-
-if(specialChars && password.length < passLength){
-    var indexSpecial= Math.floor(Math.random() * specialArray.length);
-    password = password + specialArray[indexSpecial];
-}
-if(lowerChars && password.length < passLength){
-    var indexLower = Math.floor(Math.random() * alphabet.length);
-    password = password + alphabet[indexLower];
-}
-
-if(upperChars && password.length < passLength){
-    var indexUpper = Math.floor(Math.random() * alphabet.length);
-    password = password + alphabet[indexUpper].toUpperCase();
-}
-
-if(numbers && password.length < passLength){
-    var indexNumber = Math.floor(Math.random() * numlist.length);
-    password = password + numlist[indexNumber];
-}
-
-}
-return password;
-}
-
